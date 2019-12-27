c053ab9ae8f94637936d3260da3de234a33c0372
Richard <Richard>
2019-12-27 09:45:31 -0800

    rename

--- examples/observable_attributes/src/lib.rs
+++ examples/observable_attributes/src/lib.rs
@@ -13,12 +13,16 @@ impl CustomElement for HelloPerson {
     }
 
     fn connected(&mut self) {
-        let first_name = get_attribute(&self.0, "first_name").unwrap_or("human".to_string());
-        let msg = "Hello ".to_string() + &first_name;
-        set_html(&self.0, &msg);
+        self.render();
     }
 
     fn attribute_changed(&mut self, _name: JSValue, _old_value: JSValue, _new_value: JSValue) {
+        self.render();
+    }
+}
+
+impl HelloPerson {
+    fn render(&mut self){
         let first_name = get_attribute(&self.0, "first_name").unwrap_or("human".to_string());
         let msg = "Hello ".to_string() + &first_name;
         set_html(&self.0, &msg);
